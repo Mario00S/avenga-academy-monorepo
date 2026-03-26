f46b5db8357812862d4ab3a3d28676cadf837e86
Mario Simonovski <118250007+Mario00S@users.noreply.github.com>
2026-03-26 20:36:31 +0100

    Adding first part of the Homework-Class05

---
+++ Class05.Homework/Models/Human.cs
@@ -0,0 +1,27 @@
+﻿namespace Class05.Homework.Models
+{
+    public class Human
+    {
+
+        public Human()
+        {
+            
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public Human(string fName, string lName, int age)
+        {
+            FirstName = fName;
+            LastName = lName;
+            Age = age;
+        }
+
+        public string GetPersonStats()
+        {
+            return $"First Name:{FirstName} \nLast Name:{LastName} \nAge:{Age}";
+        }
+    }
+}
---
+++ Class05.Homework/Program.cs
@@ -0,0 +1,32 @@
+﻿using Class05.Homework.Models;
+
+#region Exercise 1 requirements
+
+//Create a class Human.
+
+//Add properties:
+
+//FirstName
+//LastName
+//Age
+//Create a method GetPersonStats that:
+
+//Returns the full name
+//Returns the age
+//Ask the user for input, create an object and print the result in the console.
+
+#endregion
+Console.WriteLine("Please enter your Name, Last Name and Age");
+
+Console.WriteLine("Enter your First Name");
+string personFirstName = Console.ReadLine();
+Console.WriteLine("Enter your Last Name");
+string personLastName = Console.ReadLine();
+Console.WriteLine("Enter your age");
+bool personAge = int.TryParse(Console.ReadLine(), out int personAgeInt);
+
+Console.WriteLine("Press Enter to reveal your details");
+Console.ReadLine();
+Human newPerson = new Human(personFirstName, personLastName, personAgeInt);
+
+Console.WriteLine(newPerson.GetPersonStats());
