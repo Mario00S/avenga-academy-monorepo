7cf9c6dabcd3f32f2a81e98a056d96eb66e0ab23
Mario Simonovski <118250007+Mario00S@users.noreply.github.com>
2026-05-13 17:24:56 +0200

    homework class03 part1

---
+++ csharp-advanced/Homeworks/Class03/Class03.Homework01/Data/UserDatabase.cs
@@ -0,0 +1,51 @@
+﻿namespace Class03.Homework01.Data;
+
+public static class UserDatabase
+{
+    public static List<User> Users { get; set; } = new List<User>();
+
+    //static method should be the same name as the class
+    static UserDatabase()
+    {
+
+
+        Users = new List<User>()
+            {
+                new User {Id = 1, Name = "bobsky", Age = 25},
+                new User {Id = 2, Name = "john", Age = 35},
+            };
+    }
+
+    //additional method for retriving the users
+    public static void ListUsers()
+    {
+        for (int i = 0; i < Users.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {Users[i].Name}");
+        }
+    }
+
+    public static User Search(string name)
+    {
+        foreach (User user in Users)
+        {
+            if (user.Name == name)
+            {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public static User Search(int id)
+    {
+        foreach (User user in Users)
+        {
+            if (user.Id == id)
+            {
+                return user;
+            }
+        }
+        return null;
+    }
+}
