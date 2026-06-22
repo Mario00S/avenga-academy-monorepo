e9d5d588f794a02487088a425df1b610467e5185
Mario Simonovski <118250007+Mario00S@users.noreply.github.com>
2026-06-22 16:54:12 +0200

    adding additional Controller Views and Models

---
+++ csharp-aspnetmvc/LearningMvc01/FirstMvcApp/Controllers/CategoriesController.cs
@@ -0,0 +1,29 @@
+﻿using FirstMvcApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMvcApp.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var categories = CategoriesRepository.GetCategories();
+            return View(categories);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            var category = new Category { CategoryId = id.HasValue?id.Value:0 };
+
+            return View(category);
+            //if (id.HasValue)
+            //{
+            //    return new ContentResult { Content = id.ToString() };
+            //}
+            //else
+            //{
+            //    return new ContentResult { Content = "null content" };
+            //}
+        }
+    }
+}
