a627b61f65bab34c088784b3cb4e25420546cba6
Mario Simonovski <118250007+Mario00S@users.noreply.github.com>
2026-06-30 23:10:31 +0200

    Products Feature Added

---
+++ csharp-aspnetmvc/LearningMvc01/FirstMvcApp/Controllers/ProductsController.cs
@@ -0,0 +1,69 @@
+﻿using FirstMvcApp.Models;
+using FirstMvcApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMvcApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            var products = ProductsRepository.GetProducts();
+
+            var productViewModels = products
+                .Select(p => new ProductViewModel(p))
+                .ToList();
+
+            return View(productViewModels);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            var categories = CategoriesRepository.GetCategories();
+            ViewBag.Categories = categories;
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Add(Product product)
+        {
+            ProductsRepository.AddProduct(product);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var product = ProductsRepository.GetProductById(id);
+            if (product == null) return NotFound();
+
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, Product product)
+        {
+            ProductsRepository.UpdateProduct(id, product);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var product = ProductsRepository.GetProductById(id);
+            if (product == null) return NotFound();
+
+            return View(product);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            ProductsRepository.DeleteProduct(id);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ csharp-aspnetmvc/LearningMvc01/FirstMvcApp/ViewModels/ProductViewModel.cs
@@ -0,0 +1,30 @@
+﻿using FirstMvcApp.Models;
+
+namespace FirstMvcApp.ViewModels;
+
+public class ProductViewModel
+{
+    public int ProductId { get; set; }
+    public string ProductName { get; set; }
+    public string CategoryName { get; set; }
+    public double Price { get; set; }
+    public int Quantity { get; set; }
+
+    public ProductViewModel(Product product)
+    {
+        ProductId = product.ProductId;
+        ProductName = product.Name;
+        Price = (int)(product.Price ?? 0);
+        Quantity = product.Quantity ?? 0;
+
+        if (product.CategoryId.HasValue)
+        {
+            var category = CategoriesRepository.GetCategoryById(product.CategoryId.Value);
+            CategoryName = category?.Name ?? "Unknown";
+        }
+        else
+        {
+            CategoryName = "Unknown";
+        }
+    }
+}
