9d792b29f50981d93f50a98f65a3efe72eafd74c
Mario Simonovski <118250007+Mario00S@users.noreply.github.com>
2026-07-07 23:22:19 +0200

    to do app initial commit

---
+++ csharp-aspnetmvc/ToDo/ToDo.App/Models/Filters.cs
@@ -0,0 +1,30 @@
+﻿namespace ToDo.App.Models;
+
+public class Filters
+{
+    public Filters(string filterstring) {
+        Filterstring = filterstring ?? "all-all-all";
+        string[] filters = Filterstring.Split('-');
+        CategoryId = filters[0];
+        Due = filters[1];
+        StatusId = filters[2];
+    }
+
+    public string Filterstring { get; set; }
+    public string CategoryId { get; set; }
+    public string Due { get; set; }
+    public string StatusId { get; set; }
+    public bool HasCategory => CategoryId.ToLower() != "all";
+    public bool HasDue => Due.ToLower() != "all";
+    public static Dictionary<string, string> DueFilterValues =>
+        new Dictionary<string, string> {
+        {"future", "Future"},
+        {"past", "Past"},
+        {"today", "Today"}
+    };
+
+    public bool IsPast => Due.ToLower() == "past";
+    public bool IsFuture => Due.ToLower() == "future";
+    public bool IsToday => Due.ToLower() == "today";
+
+}
---
+++ csharp-aspnetmvc/ToDo/ToDo.App/Models/Status.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDo.App.Models
+{
+    public class Status
+    {
+        public string StatusId { get; set; } = string.Empty;
+        public string Name { get; set; }
+
+    }
+}
