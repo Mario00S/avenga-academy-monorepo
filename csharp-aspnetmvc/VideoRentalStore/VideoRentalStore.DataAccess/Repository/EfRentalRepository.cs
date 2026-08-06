16cac6f3f6596cf15c7749916e71e2dad502473c
Mario Simonovski <118250007+Mario00S@users.noreply.github.com>
2026-08-06 02:10:30 +0200

    Migration 50% completed

---
+++ csharp-aspnetmvc/VideoRentalStore/VideoRentalStore.DataAccess/Repository/EfRentalRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VideoRentalStore.DataAccess.Interfaces;
+using VideoRentalStore.Domain.Entities;
+
+namespace VideoRentalStore.DataAccess.Repository
+{
+    public class EfRentalRepository : EfRepository<Rental>, IRentalRepository
+    {
+        private readonly VideoRentalDbContext _context;
+
+        public EfRentalRepository(VideoRentalDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Rental> GetByUserId(int userId)
+        {
+            return _context.Rentals
+                .Where(r => r.UserId == userId)
+                .ToList();
+        }
+
+        public IEnumerable<Rental> GetByMovieId(int movieId)
+        {
+            return _context.Rentals
+                .Where(r => r.MovieId == movieId)
+                .ToList();
+        }
+
+        public int GetMonthlyRentalCount(int userId, DateTime monthReference)
+        {
+            return _context.Rentals
+                .Where(r => r.UserId == userId &&
+                            r.RentedOn.Year == monthReference.Year &&
+                            r.RentedOn.Month == monthReference.Month)
+                .Count();
+        }
+    }
+}
